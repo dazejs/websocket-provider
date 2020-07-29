5371591ab15ab10b91e1195fc1d4acc10c9d366f
Zewail <chanzewail@gmail.com>
2020-07-29 09:40:11 +0800

    feat(deps): update dazejs version to 3.4.1 (#101)
    Co-authored-by: zewail <zewail@zewaildeMacBook-Pro.local>

--- src/websocket-service-provider.ts
+++ src/websocket-service-provider.ts
@@ -1,41 +1,45 @@
-import { BaseProvider, Loader, inject, provide, Application } from '@dazejs/framework';
+import { BaseProvider, Loader, provide, Application, disable } from '@dazejs/framework';
 import { Websocket } from './websocket-service';
 import * as symbols from './symbols';
 import sio from 'socket.io';
 import { Response } from './response';
 
 export class WebsocketServiceProvider extends BaseProvider {
-  @inject('loader') loader: Loader;
 
   @provide('websocket')
+  @disable
   _websocket(app: Application) {
     return new Websocket(app);
   }
 
   @provide(symbols.INJECTORS.SOCKET, false)
+  @disable
   _socket(socket: sio.Socket) {
     return socket;
   }
 
   @provide(symbols.INJECTORS.MESSAGE, false)
+  @disable
   _message(_socket: sio.Socket, args: any[]) {
     const [msg] = args;
     return msg;
   }
 
   @provide(symbols.INJECTORS.MESSAGES, false)
+  @disable
   _messages(_socket: sio.Socket, args: any[]) {
     return args;
   }
 
   @provide(symbols.INJECTORS.RESPONSE, false)
+  @disable
   _response() {
     return new Response();
   }
 
   launch() {
     const ws = this.app.get<Websocket>('websocket');
-    const Components = this.loader.getComponentsByType('websocket') || [];
+    const Components = this.app.get<Loader>('loader').getComponentsByType('websocket') || [];
     for (const Component of Components) {
       const name = Reflect.getMetadata('name', Component);
       this.app.multiton(Component, Component);
